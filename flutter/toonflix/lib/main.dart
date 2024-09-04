3e52f5b479c83681615402c5af570aa491850601
sseung519 <htc_desirehd@naver.com>
2024-09-04 16:46:21 +0900

    flutter

---
+++ flutter/toonflix/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, flutter!'),
+        ),
+        body: Center(child: Text('Hello, World!'),
+        ),
+      ),
+    );  
+  }
+  
+}
+
+
